8e14e488eb1628e7761e9fb9e034bcfa99c52381
donb01326-hue <donb01326@gmail.com>
2026-04-28 16:23:34 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -116,12 +116,12 @@ const BuyBPCPayment = () => {
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">5249074646</p>
+            <p className="text-lg font-bold">5470075531</p>
             <Button
               variant="default"
               size="sm"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-1"
-              onClick={() => handleCopy("5249074646", "Account Number")}
+              onClick={() => handleCopy("5470075531", "Account Number")}
             >
               <Copy size={14} />
               Copy
@@ -131,12 +131,12 @@ const BuyBPCPayment = () => {
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Bank Name</p>
-          <p className="text-lg font-bold">MONIEPOINT MFB</p>
+          <p className="text-lg font-bold">ECOBANK</p>
         </div>
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Name</p>
-          <p className="text-lg font-bold">FLORENCE BOSEDE GBOLAJOGUN</p>
+          <p className="text-lg font-bold">GBENGA OLORUNWA GBOLAJOGUN</p>
         </div>
       </div>
 
